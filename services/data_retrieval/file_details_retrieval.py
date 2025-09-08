09c9771ef76550088a39515b347e0416401fc82c
Aharon Frenkel <frenkelahron@gmail.com>
2025-09-08 16:31:08 +0300

    refactor: Improving the file retrieval class
    - Renaming the class from FileRetrieval to FileDetailsRetrieval
    - Loading the file metadata on object initialization
    - Changing the methods for obtaining file details from methods to properties

--- services/data_retrieval/file_details_retrieval.py
+++ services/data_retrieval/file_details_retrieval.py
@@ -2,26 +2,21 @@ from pathlib import Path
 from typing import Any
 
 
-class FileRetrieval:
+class FileDetailsRetrieval:
     def __init__(self, file_path: str):
         """Initialize the FileRetrieval object"""
         self._file_path = self._validate_file_path(file_path)
+        self._file_metadata = self._file_path.stat()
 
-    def get_file_path(self) -> str:
+    @property
+    def path(self) -> str:
         """Get the file path as a string"""
-        return str(self._file_path)
+        return self._file_path
 
-    def get_file_metadata(self) -> dict[str, Any]:
+    @property
+    def metadata(self) -> dict[str, Any]:
         """Get the file metadata as a dictionary"""
-        file_metadata = self._file_path.stat()
-
-        result = {
-            "file_name": self._file_path.name,
-            "file_size": file_metadata.st_size,
-            "file_creation_timestamp": file_metadata.st_ctime_ns
-        }
-
-        return result
+        return self._file_metadata
 
     @staticmethod
     def _validate_file_path(file_path) -> Path:
@@ -42,11 +37,3 @@ class FileRetrieval:
             raise ValueError("Path is not a file")
 
         return path
-
-git_commit = """
-add file retrieval class
-
-- add FileRetrieval class
-- add get_file_path method
-- add get_file_metadata method
-"""
